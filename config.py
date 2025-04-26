2b649df5516e779ce5feeb0744828af291d79f3f
rnpublic88628867 <rnpublicschool7761@gmail.com>
2025-04-26 08:18:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "29685534"
+api_hash = "5204f93bb78feb40bedf61cad13df543"
+bot_token = "7808700536:AAGZIbigjvtMGJhxVYDrL-MQ3JOR5A68Au4"
+auth_users = [7890986967]
+sudo_users = [7890986967]
